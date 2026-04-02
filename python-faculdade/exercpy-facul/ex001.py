3f2f26ab9fb015e3fed46a7a6906041e55ab28fb
Guilherme Lindner <glindnerp@proton.me>
2026-04-02 14:52:40 -0400

    Deletando arquivos desnecessários

--- python-faculdade/exercpy-facul/ex001.py
+++
@@ -1,4 +0,0 @@
-#Conversor de temperaturas (F - C)
-tempf = float(input('Digite o valor da temperatura em Fahreheit: '))
-tempc = (tempf - 32) * 5/9
-print(f'Sua temperatura em Celsius é igual a:  {tempc:.2f}C')
--- python-faculdade/exercpy-facul/ex004.py
+++
@@ -1,3 +0,0 @@
-valor = (input('Digite um valor de 4 dígitos: '))
-invertido = valor[::-1]
-print( f'O valor invertido é {invertido}')
